ec81c53d275c00486c3e6c0e84aa3193e62bf7bb
Magyster <adamrudnik23@gmail.com>
2020-12-19 21:26:14 +0100

    dodanie css

--- sterownik_piec/views.py
+++ sterownik_piec/views.py
@@ -3,5 +3,7 @@ from .models import Czujniki
 
 def glowna(request):
     czujniki = Czujniki.objects.all()
-    #czujniki = Czujniki.objects.filter(nazwa='T_dom_A')
-    return render(request, 'sterownik_piec/glowna.html', {'czujniki':czujniki})
+    for czujnik in czujniki:
+        if czujnik.nazwa == 'T_dom_A':
+            czujnik_T_dom_A = czujnik.wartosc#Czujniki.objects.filter(nazwa='T_dom_A')
+    return render(request, 'sterownik_piec/glowna.html', {'czujnik_T_dom_A':czujnik_T_dom_A,})
