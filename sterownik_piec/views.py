88a4a128027fe3595e6a4e9badd4867b4ea22420
Magyster <adamrudnik23@gmail.com>
2020-12-19 18:04:52 +0100

    zmiany pliku html

--- sterownik_piec/views.py
+++ sterownik_piec/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Czujniki
 
 def glowna(request):
-    return render(request, 'sterownik_piec/glowna.html', {})
+    czujniki = Czujniki.objects.all()
+    #czujniki = Czujniki.objects.filter(nazwa='T_dom_A')
+    return render(request, 'sterownik_piec/glowna.html', {'czujniki':czujniki})
