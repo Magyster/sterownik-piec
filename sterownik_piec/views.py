946a50f2e0ef1a48b74ece7d6b78630002334db4
Magyster <adamrudnik23@gmail.com>
2021-01-02 19:21:50 +0100

    02_01_2020

--- sterownik_piec/views.py
+++ sterownik_piec/views.py
@@ -22,11 +22,19 @@ def glowna(request):
 
 def zapis_do_bazy(request):
     if request.method == 'POST':
+        #zapis wartości dla przesłanych czujników
         czujniki = json.loads(request.body)
         for klucz, wartosc in czujniki.items():
             czujnik = Czujniki.objects.get(nazwa = klucz)
             czujnik.wartosc = wartosc
             czujnik.save()
+        #obliczenie tem zadanej grzejników
+        czujnik = Czujniki.objects.get(nazwa = 'T_grzejniki_Z')
+        t_dom = Czujniki.objects.get(nazwa='T_dom_A')
+        t_zewnetrzna = Czujniki.objects.get(nazwa='T_zewnetrzna_A')
+        wartosc = (-1 * t_dom.wartosc + 85) - t_zewnetrzna.wartosc
+        czujnik.wartosc = wartosc
+        czujnik.save()
     return JsonResponse(None, safe=False)
 
 def odczyt_z_bazy(request):
