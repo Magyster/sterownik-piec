f65cd281891911507596e3b5f97080a41f46eaa1
Magyster <adamrudnik23@gmail.com>
2021-01-05 17:15:10 +0100

    05_01_2020

--- sterownik_piec/urls.py
+++ sterownik_piec/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.glowna, name='glowna'),
-    path('zapisdobazy/', views.zapis_do_bazy, name='zapis_do_bazy'),
-    path('odczytzbazy/', views.odczyt_z_bazy, name='odczyt_z_bazy'),
+    path('', views.Glowna.as_view(), name='Glowna'),
+    path('ZapisDoBazy/', views.ZapisDoBazy.as_view(), name='ZapisDoBazy'),
+    path('OdczytZBazy/', views.OdczytZBazy.as_view(), name='OdczytZBazy'),
 ]
--- sterownik_piec/views.py
+++ sterownik_piec/views.py
@@ -1,45 +1,80 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.http import JsonResponse
+from django.views import View
 from .models import Czujniki
 import json
 import datetime
 
-def glowna(request):
-    czujniki = Czujniki.objects.all()
-    dane = {}
-    teraz = datetime.datetime.now()
-    data = teraz.date()
-    czas = teraz.time()
-    dane['data'] = data
-    dane['czas'] = czas
-    for czujnik in czujniki:
-        if(czujnik.typ == 'T'):
-            dane[czujnik.nazwa] =  round(czujnik.wartosc,1)#Czujniki.objects.filter(nazwa='T_dom_A')
-        else:
-            dane[czujnik.nazwa] =  int(czujnik.wartosc)
-        
-    return render(request, 'sterownik_piec/glowna.html', dane)
+class Glowna(View):
 
-def zapis_do_bazy(request):
-    if request.method == 'POST':
+    def get(self, request):
+        czujniki = Czujniki.objects.all()
+        dane = {}
+        teraz = datetime.datetime.now()
+        data = teraz.date()
+        czas = teraz.time()
+        dane['data'] = data
+        dane['czas'] = czas
+        for czujnik in czujniki:
+            if(czujnik.typ == 'T'):
+                dane[czujnik.nazwa] =  round(czujnik.wartosc,1)
+            else:
+                dane[czujnik.nazwa] =  int(czujnik.wartosc)        
+        return render(request, 'sterownik_piec/glowna.html', dane)
+
+    def post(self, request):
+        czujniki = request.POST        
+        for klucz, wartosc in czujniki.items():
+            if(klucz != 'csrfmiddlewaretoken'):
+                czujnik = Czujniki.objects.get(nazwa = klucz)
+                czujnik.wartosc = wartosc
+                czujnik.save()
+
+        czujniki = Czujniki.objects.all()
+        dane = {}
+        teraz = datetime.datetime.now()
+        data = teraz.date()
+        czas = teraz.time()
+        dane['data'] = data
+        dane['czas'] = czas
+        for czujnik in czujniki:
+            if(czujnik.typ == 'T'):
+                dane[czujnik.nazwa] =  round(czujnik.wartosc,1)
+            else:
+                dane[czujnik.nazwa] =  int(czujnik.wartosc)        
+        return render(request, 'sterownik_piec/glowna.html', dane)
+
+
+class ZapisDoBazy(View):
+
+    def get(self, request):
+        pass        
+
+    def post(self, request):
         #zapis wartości dla przesłanych czujników
         czujniki = json.loads(request.body)
         for klucz, wartosc in czujniki.items():
             czujnik = Czujniki.objects.get(nazwa = klucz)
             czujnik.wartosc = wartosc
             czujnik.save()
-        #obliczenie tem zadanej grzejników
+        #obliczenie temperatury zadanej grzejników
         czujnik = Czujniki.objects.get(nazwa = 'T_grzejniki_Z')
         t_dom = Czujniki.objects.get(nazwa='T_dom_A')
         t_zewnetrzna = Czujniki.objects.get(nazwa='T_zewnetrzna_A')
-        wartosc = (-1 * t_dom.wartosc + 85) - t_zewnetrzna.wartosc
+        wartosc = (-1 * t_dom.wartosc + 85) - t_zewnetrzna.wartosc / 2
         czujnik.wartosc = wartosc
         czujnik.save()
-    return JsonResponse(None, safe=False)
+        return JsonResponse(None, safe=False)
 
-def odczyt_z_bazy(request):
-    czujniki = Czujniki.objects.all()
-    dane = {}
-    for czujnik in czujniki:
-        dane.update({czujnik.nazwa:czujnik.wartosc})        
-    return JsonResponse(dane, safe=False)
+
+class OdczytZBazy(View):
+
+    def get(self, request):
+        czujniki = Czujniki.objects.all()
+        dane = {}
+        for czujnik in czujniki:
+            dane.update({czujnik.nazwa:czujnik.wartosc})        
+        return JsonResponse(dane, safe=False)
+
+    def post(self, request):
+        pass
